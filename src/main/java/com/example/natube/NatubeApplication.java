7f560f6e38fcfb3952184c14f783ad9cca0b616f
Kimseungsuu <107012988+Kimseungsuu@users.noreply.github.com>
2022-07-23 10:38:18 +0900

    프로젝트 시작

---
+++ src/main/java/com/example/natube/NatubeApplication.java
@@ -0,0 +1,13 @@
+package com.example.natube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NatubeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/natube/NatubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.natube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
